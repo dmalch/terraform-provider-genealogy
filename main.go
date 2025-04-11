882854001dc3cb368f3badf4a1a896d1976aa522
Dmitry Malchikov <dmalch@gmail.com>
2025-04-10 21:39:59 -0500

    Add debug flag support for provider execution

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"log"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
@@ -10,9 +11,16 @@ import (
 )
 
 func main() {
+	var debug bool
+
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
 	err := providerserver.Serve(context.Background(), internal.New, providerserver.ServeOpts{
 		Address: "registry.terraform.io/dmalch/genealogy",
+		Debug:   debug,
 	})
+
 	if err != nil {
 		log.Fatal(err)
 	}
